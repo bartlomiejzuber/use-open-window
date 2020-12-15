de14c393137133d0863ba3b7c218b17c133e73ac
Bart Zuber <bartlomiej.zuber@outlook.com>
2020-12-15 18:07:40 +0100

    add demo

---
+++ demo/src/App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { useOpenInWindow }  from 'use-open-window';
+
+const url = 'https://www.google.com/';
+const options = {
+   centered: true, /* default */
+   spec: {
+      width: 800, /* window width */
+      height: 600, /* window height */
+   }
+};
+
+const App = () => {
+  const [handleWindowOpen, newWindowHandle] = useOpenInWindow(url, options);
+  
+  return (
+    <div className="App">
+      <div onClick={handleWindowOpen}>Click me to open new browser window</div>
+    </div>
+  );
+}
+
+export default App;
